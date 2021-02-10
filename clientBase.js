92ac8dd0c60a0d657779855a19f72d3ded9c85ba
Skelly Mclane <74203498+SkellyM386@users.noreply.github.com>
2021-02-09 18:07:36 -0800

    started custom registry commands
    added a <commands>.custom.help(); returns an array of the groups and the commands within them.
    also added a <commands>.registerHelpCommand(); that automatically creates a help command for you.

--- clientBase.js
+++ clientBase.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
+const Duration = require('humanize-duration');
+
 
 const fs = require('fs');
+const { type } = require('os');
+const { execute } = require('./commands/ping');
 
 module.exports = class AleshClient extends Discord.Client {
   constructor(clientOptions) {
@@ -9,6 +13,7 @@ module.exports = class AleshClient extends Discord.Client {
     class CommandRegistry {
       constructor(commandFolder) {
         this.commands = new Discord.Collection();
+        this.cooldown = null;
 
         const commandFiles = fs.readdirSync(`./${commandFolder}/`).filter(file => (file.endsWith('.js') || !file.includes('.') && file !== 'procfile'));
 
@@ -17,8 +22,81 @@ module.exports = class AleshClient extends Discord.Client {
 
           this.commands.set(command.name, command);
         };
-      }
+
+        this.commandFilter = (commands = this.commands, command) => {
+          const usableCommands = commands.filter(cmd => {
+            if(cmd.name)if(cmd.name === command)return true;
+            if(cmd.aliases)if(cmd.aliases.includes(command))return true;
+            return false;
+        });
+        return usableCommands;
+      };
+
+      this.commands.run = (message = new Discord.Message, command, req) => {
+        const cmd = this.commandFilter(this.commands, command).first();
+        if(cmd)return cmd.execute(req);
+        return;
+      };
+
+      this.custom = {
+        help: () => {
+          let commands = [];
+          let pushContent = (obj = new Object, property = String()) => {
+            return (obj[property] ? obj[property]: undefined);
+          };
+          for(const cmd of this.commands.array().filter(val => typeof val === 'object')){
+            commands.push({
+              name: pushContent(cmd, 'name'),
+              memberName: (cmd['memberName'] ? cmd['memberName']: (cmd['name'] ? cmd['name']: null)),
+              description: pushContent(cmd, 'description'),
+              examples: pushContent(cmd, 'examples'),
+              group: pushContent(cmd, 'group'),
+              theme: pushContent(cmd, 'theme'),
+            });
+          };
+
+          let commandGroups = [];
+
+          this.commands.forEach(command => {
+            if(!commandGroups.includes(command.group))commandGroups.push({ name: command.group, commands: [] });
+          });
+          this.commands.forEach(command => {
+            if(command.group){
+              commandGroups.find(group => group.name === command.group).commands.push(command.name);
+            }
+          });
+          if(commandGroups.find(group => group.name === 'other').commands.length < 1)commandGroups.shift();
+          return commandGroups;
+        }
+      };
+
+      const commands = this.commands;
+      const custom = this.custom;
+
+      this.registerHelpCommand = () => {
+        this.commands.set('help', {
+          name: "help",
+          group: "other",
+          description: "show what commands are on this bot",
+          execute({ message } = req){
+      let embed = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setAuthor(message.author.username, message.author.avatarURL());
+      const groups = custom.help();
+      for(const group of groups){
+        let groupCommands = [];
+        for(const cmd of group.commands)groupCommands.push(cmd.name);
+        let stringOfCommands = `\`${group.commands.join('\`, \`')}\``;
+        embed.addField(`__**${group.name}**__`, stringOfCommands)
+      };
+      message.channel.send(embed);
+        }
+    })
+  }
+
     };
+   };
+
 
     this.createRegistry = (commandFolder) => new CommandRegistry(commandFolder);
 
