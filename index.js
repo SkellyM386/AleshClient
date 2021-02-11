18118edd1c390981bd50b73d1869bd33455c8fa5
Skelly Mclane <74203498+SkellyM386@users.noreply.github.com>
2021-02-11 11:59:24 -0800

    apart of the plugin and play update

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-const Discord = require('discord.js');
+const Client = require('./clientBase');
 
-const AleshClient = require('./clientBase');
-
-const client = new AleshClient();
-
-const prefix = '.';
-
-const commands = client.createRegistry('commands');
-
-client.login(require('./config.json').token).catch(console.log)
-
-client.on('ready', () => console.log(`${client.user.username} has logged in!`))
-
-commands.registerHelpCommand();
-
-client.on("message", async message => {
-  const { content, author } = message;
-  if (!content.startsWith(prefix) || author.bot) return;
-
-  const args = content.slice(prefix.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
-
-  const req = {
-    client: client,
-    message: message,
-    args: args,
-    command: command,
-    Discord: Discord,
-  };
-  commands.commands.run(message, command, req);
-
-
-});
+const client = new Client({}, 'commands', require('./config.json'));
